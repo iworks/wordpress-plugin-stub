22df3d6c90e377fe37508d0e72543e8f72b70397
Marcin Pietrzak <marcin@iworks.pl>
2018-04-08 14:30:58 +0200

    init

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== 5O5 ===
+Contributors: iworks
+Donate link: http://iworks.pl/donate/5o5.php
+Tags: 5o5, dinghy, boat, crew, team
+Requires at least: 3.9
+Tested up to: 4.9
+Stable tag: 0.0
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+
+== Description ==
+
+== Installation ==
+
+There are 3 ways to install this plugin:
+
+= 1. The super easy way =
+1. In your Admin, go to menu Plugins > Add
+1. Search for `5O5`
+1. Click to install
+1. Activate the plugin
+1. A new menu `5O5` will appear in your Admin
+
+= 2. The easy way =
+1. Download the plugin (.zip file) on the right column of this page
+1. In your Admin, go to menu Plugins > Add
+1. Select button `Upload Plugin`
+1. Upload the .zip file you just downloaded
+1. Activate the plugin
+1. A new menu `5O5` will appear in your Admin
+
+= 3. The old and reliable way (FTP) =
+1. Upload `5o5` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. A new menu `5O5` will appear in your Admin
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+== Upgrade Notice ==
+
---
+++ wordpress-plugin-stub.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: wordpress-plugin-stub.pot
+Text Domain: wordpress-plugin-stub.pot
+Plugin URI: http://iworks.pl/wordpress-plugin-stub.pot/
+Description: 
+Version: PLUGIN_VERSION
+Author: Marcin Pietrzak
+Author URI: http://iworks.pl/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright 2018 Marcin Pietrzak (marcin@iworks.pl)
+
+this program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * static options
+ */
+define( 'IWORKS_WORDPRESS_PLUGIN_STUB_VERSION', 'PLUGIN_VERSION' );
+define( 'IWORKS_WORDPRESS_PLUGIN_STUB_PREFIX',  'iworks_wordpress-plugin-stub_' );
+$base = dirname( __FILE__ );
+$vendor = $base.'/vendor';
+
+/**
+ * require: Iworkswordpress-plugin-stub Class
+ */
+if ( ! class_exists( 'iworks_wordpress_plugin_stub' ) ) {
+	require_once $vendor.'/iworks/wordpress-plugin-stub.php';
+}
+/**
+ * configuration
+ */
+require_once $base.'/etc/options.php';
+/**
+ * require: IworksOptions Class
+ */
+if ( ! class_exists( 'iworks_options' ) ) {
+	require_once $vendor.'/iworks/options/options.php';
+}
+
+/**
+ * i18n
+ */
+load_plugin_textdomain( 'wordpress-plugin-stub', false, plugin_basename( dirname( __FILE__ ) ).'/languages' );
+
+/**
+ * load options
+ */
+$iworks_wordpress_plugin_stub_options = new iworks_options();
+$iworks_wordpress_plugin_stub_options->set_option_function_name( 'iworks_wordpress_plugin_stub_options' );
+$iworks_wordpress_plugin_stub_options->set_option_prefix( IWORKS_WORDPRESS_PLUGIN_STUB_PREFIX );
+
+function iworks_wordpress_plugin_stub_options_init() {
+	global $iworks_wordpress_plugin_stub_options;
+	$iworks_wordpress_plugin_stub_options->options_init();
+}
+
+function iworks_wordpress_plugin_stub_activate() {
+	$iworks_wordpress_plugin_stub_options = new iworks_options();
+	$iworks_wordpress_plugin_stub_options->set_option_function_name( 'iworks_wordpress_plugin_stub_options' );
+	$iworks_wordpress_plugin_stub_options->set_option_prefix( IWORKS_WORDPRESS_PLUGIN_STUB_PREFIX );
+	$iworks_wordpress_plugin_stub_options->activate();
+}
+
+function iworks_wordpress_plugin_stub_deactivate() {
+	global $iworks_wordpress_plugin_stub_options;
+	$iworks_wordpress_plugin_stub_options->deactivate();
+}
+
+$iworks_wordpress_plugin_stub = new iworks_wordpress_plugin_stub();
+
+/**
+ * install & uninstall
+ */
+register_activation_hook( __FILE__,   'iworks_wordpress_plugin_stub_activate' );
+register_deactivation_hook( __FILE__, 'iworks_wordpress_plugin_stub_deactivate' );
